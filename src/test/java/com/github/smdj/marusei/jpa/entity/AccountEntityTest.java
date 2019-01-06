834ee4737266a7b015e02c4057cc3c095ed6edcd
hellosamuel <samuelis419@gmail.com>
2019-01-06 18:34:40 +0900

    ADD 회원등록 테스트 작성 완료

---
+++ src/test/java/com/github/smdj/marusei/jpa/entity/AccountEntityTest.java
@@ -0,0 +1,34 @@
+package com.github.smdj.marusei.jpa.entity;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccountEntityTest {
+    private static final Logger log = LoggerFactory.getLogger(AccountEntityTest.class);
+
+    @Test
+    public void test_new() {
+        // given
+        String email = "hellosamuel@google.com";
+        String nickname = "hellosamuel";
+        Instant createdAt = Instant.now();
+
+        log.debug("Method_Step = {}, email = {}, nickname = {}, createdAt = {}", "given", email, nickname, createdAt);
+
+        // when
+        AccountEntity accountEntity = new AccountEntity(email, nickname, createdAt);
+
+        log.debug("Method_Step = {}, accountEntity = {}", "when", accountEntity);
+
+        // then
+        assertThat(accountEntity)
+                .isNotNull()
+                .extracting(AccountEntity::getId, AccountEntity::getEmail, AccountEntity::getNickname, AccountEntity::getCreatedAt, AccountEntity::getUpdatedAt)
+                .containsSequence(0, email, nickname, createdAt, createdAt);
+    }
+}
