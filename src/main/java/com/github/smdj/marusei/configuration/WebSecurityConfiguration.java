d1bf222be69d8a597e1d548c0bf4d516879311da
hellosamuel <samuelis419@gmail.com>
2019-01-09 21:08:13 +0900

    EDIT 게스트가 회원가입페이지에 접근할 수 있도록 허가

--- src/main/java/com/github/smdj/marusei/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/github/smdj/marusei/configuration/WebSecurityConfiguration.java
@@ -19,6 +19,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin();
         http.authorizeRequests().antMatchers("/", "/signup").anonymous();
     }
 }
