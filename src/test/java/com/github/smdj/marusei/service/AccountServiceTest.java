76acbfefe56742a13e6d42f314d818f8a7ccd5ff
hellosamuel <samuelis419@gmail.com>
2019-01-09 20:45:47 +0900

    EDIT AccountService / Rollback 어노테이션 주석처리

--- src/test/java/com/github/smdj/marusei/service/AccountServiceTest.java
+++ src/test/java/com/github/smdj/marusei/service/AccountServiceTest.java
@@ -14,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,7 +26,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Launcher.class)
 @Transactional
-@Rollback(false)
+//@Rollback(false)
 public class AccountServiceTest {
     private static final Logger log = LoggerFactory.getLogger(AccountServiceTest.class);
 
