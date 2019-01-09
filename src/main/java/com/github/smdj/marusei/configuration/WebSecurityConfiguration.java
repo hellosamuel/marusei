29da2f2b101d0331f84e23818eaa1b6520fc9c23
hellosamuel <samuelis419@gmail.com>
2019-01-09 21:04:52 +0900

    EDIT 회원가입 처리 완료

--- src/main/java/com/github/smdj/marusei/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/github/smdj/marusei/configuration/WebSecurityConfiguration.java
@@ -2,17 +2,23 @@ package com.github.smdj.marusei.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/", "/signup").anonymous();
+    }
 }
