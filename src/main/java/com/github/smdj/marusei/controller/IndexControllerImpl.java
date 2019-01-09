7e79ec63e266019228854f91420154bd2d25a37a
hellosamuel <samuelis419@gmail.com>
2019-01-09 20:58:04 +0900

    EDIT 회원가입 컨트롤러 처리 추가

--- src/main/java/com/github/smdj/marusei/controller/IndexControllerImpl.java
+++ src/main/java/com/github/smdj/marusei/controller/IndexControllerImpl.java
@@ -1,8 +1,11 @@
 package com.github.smdj.marusei.controller;
 
 import com.github.smdj.marusei.controller.request.SignUpRequest;
+import com.github.smdj.marusei.service.AccountService;
+import com.github.smdj.marusei.service.params.CreateAccountParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +17,9 @@ import javax.validation.Valid;
 @Controller
 class IndexControllerImpl implements IndexController {
     private static final Logger log = LoggerFactory.getLogger(IndexControllerImpl.class);
+
+    @Autowired
+    private AccountService accountService;
 
     @Override
     public String index() {
@@ -45,6 +51,14 @@ class IndexControllerImpl implements IndexController {
             return "page/signup";
         }
 
-        return "page/login";
+        CreateAccountParams createAccountParams = new CreateAccountParams(signUpRequest.getEmail(),
+                signUpRequest.getNickname(),
+                signUpRequest.getPassword());
+
+        if (accountService.create(createAccountParams) != null) {
+            return "redirect:/login";
+        }
+
+        throw new RuntimeException("이런 회원등록에 실패 했어요! 다시 한 번만 기회를 주세요.");
     }
 }
