396c1354b68d438a3c8e40e874edfd38a304cfe8
hellosamuel <samuelis419@gmail.com>
2019-01-09 20:03:04 +0900

    EDIT AccountService / create 작성

--- src/main/java/com/github/smdj/marusei/jpa/entity/CredentialEntity.java
+++ src/main/java/com/github/smdj/marusei/jpa/entity/CredentialEntity.java
@@ -49,15 +49,15 @@ public class CredentialEntity implements Credential {
         this.updatedAt = createdAt;
     }
 
-    public CredentialEntity(Account account, String publicKey, String secreteHash, Instant createdAt) {
+    public CredentialEntity(Account account, String publicKey, String secretHash, Instant createdAt) {
         if (account == null) {
             throw new IllegalArgumentException("account is null");
         }
         if (publicKey == null) {
             throw new IllegalArgumentException("publicKey is null");
         }
-        if (secreteHash == null) {
-            throw new IllegalArgumentException("secreteHash is null");
+        if (secretHash == null) {
+            throw new IllegalArgumentException("secretHash is null");
         }
         if (createdAt == null) {
             throw new IllegalArgumentException("createdAt is null");
@@ -65,7 +65,7 @@ public class CredentialEntity implements Credential {
 
         this.account = account;
         this.publicKey = publicKey;
-        this.secreteHash = secreteHash;
+        this.secretHash = secretHash;
         this.createdAt = createdAt;
         this.updatedAt = createdAt;
     }
